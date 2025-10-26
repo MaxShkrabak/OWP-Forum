31a4de5fcb6127175070613c87e67cd22b634337
Maksim Shkrabak <shkrabakmaksim@gmail.com>
2025-10-26 00:13:01 -0700

    BB-308: Add route guard for authenticated pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@
     import RegistrationPage from '../views/Register.vue';
     import VerifyPasscode from '../views/VerifyPasscode.vue';
     import ForumUserProfile from '../views/UserProfile.vue';
+    import axios from 'axios';
 
     const routes = [
       { path: '/', name: 'ForumHome', component: HomePage },
@@ -17,6 +18,26 @@
     const router = createRouter({
       history: createWebHistory(),
       routes,
+    });
+
+    // Route guard
+    router.beforeEach(async (to, from, next) => {
+      if (to.meta.requiresAuth) {
+        // Authentication is required for the page
+        try {
+          const res = await axios.get(`${API}/api/me`, { withCredentials: true });
+          if (res.data.ok) {
+            next(); // user is logged in, route to page
+          } else {
+            next({ name: '/', query: { redirect: to.fullPath } }); // user is NOT logged in, route to home
+          }
+        } catch (e) {
+          // Backend is down or some other issue, redirect to home
+          next({ name: '/', query: { redirect: to.fullPath } });
+        }
+      } else {
+        next(); // no auth is needed for that page
+      }
     });
 
     export default router; 
