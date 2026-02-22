f71369c4f8b6dfc45c6782eadaed728ea01fbf76
Maksim Shkrabak <shkrabakmaksim@gmail.com>
2026-02-21 16:49:36 -0800

    Setup automated testing pipelines for FE and BE

---
+++ backend/tests/ConnectionTest.php
@@ -0,0 +1,9 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+// Simple test to see if backend testing works
+class ConnectionTest extends TestCase {
+    public function test_phpunit_is_running() {
+        $this->assertTrue(true);
+    }
+}
