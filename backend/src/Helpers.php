f6596cf07a26a09fa9646546916b0d17976d4c39
Maksim Shkrabak <shkrabakmaksim@gmail.com>
2026-02-22 18:24:50 -0800

    Autoload helpers

--- backend/src/Helpers.php
+++ backend/src/Helpers.php
@@ -3,31 +3,37 @@ namespace Forum\Helpers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 
-function json(Response $res, array $data, int $status = 200): Response {
-    $res->getBody()->write(json_encode($data));
-    return $res
-        ->withStatus($status)
-        ->withHeader('Content-Type', 'application/json');
+if (!function_exists('Forum\Helpers\json')) {
+    function json(Response $res, array $data, int $status = 200): Response {
+        $res->getBody()->write(json_encode($data));
+        return $res
+            ->withStatus($status)
+            ->withHeader('Content-Type', 'application/json');
+    }
 }
 
-function setSessionCookie(string $rawToken): void {
-    setcookie('session', $rawToken, [
-        'expires'  => time() + 86400, // 24 hours
-        'path'     => '/',
-        'domain'   => 'localhost',
-        'secure'   => false,
-        'httponly' => true,
-        'samesite' => 'Lax'
-    ]);
+if (!function_exists('Forum\Helpers\setSessionCookie')) {
+    function setSessionCookie(string $rawToken): void {
+        setcookie('session', $rawToken, [
+            'expires'  => time() + 86400, // 24 hours
+            'path'     => '/',
+            'domain'   => 'localhost',
+            'secure'   => false,
+            'httponly' => true,
+            'samesite' => 'Lax'
+        ]);
+    }
 }
 
-function clearSessionCookie(): void {
-    setcookie('session', '', [
-        'expires'  => time() - 3600, // One hour ago
-        'path'     => '/',
-        'domain'   => 'localhost',
-        'secure'   => false,
-        'httponly' => true,
-        'samesite' => 'Lax'
-    ]);
+if (!function_exists('Forum\Helpers\clearSessionCookie')) {
+    function clearSessionCookie(): void {
+        setcookie('session', '', [
+            'expires'  => time() - 3600, // One hour ago
+            'path'     => '/',
+            'domain'   => 'localhost',
+            'secure'   => false,
+            'httponly' => true,
+            'samesite' => 'Lax'
+        ]);
+    }
 }
